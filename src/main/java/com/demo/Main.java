e292d73285ddc7e560d8becb8d4c9b6c1ca1c654
febrynn <11221008@student.itk.ac.id>
2023-08-25 15:47:48 +0800

    i mengupdate

--- src/main/java/com/demo/Main.java
+++ src/main/java/com/demo/Main.java
@@ -1,7 +1,23 @@
 package com.demo;
 
+import com.demo.Node;
+import com.demo.Queue;
+import com.demo.Stack;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        Stack stack=new Stack();
+        Queue queue=new Queue();
+        System.out.println("tescase1: melihat stack kosong atau tidak");
+        System.out.println(stack.hasPop());
+        System.out.println("tascase2: pop value");
+        System.out.println(stack.pop());
+        stack.hasPop();
+        stack.push(0);
+        stack.pop();
+        
+
+
     }
 }
