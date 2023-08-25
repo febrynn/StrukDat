45a5cb82bf2df7fc4e327c23475ecd692ac657a0
rvyhvn <hanshitrash00@gmail.com>
2023-08-25 16:24:14 +0800

    test case di stack

--- src/main/java/com/demo/Main.java
+++ src/main/java/com/demo/Main.java
@@ -4,20 +4,53 @@ import com.demo.Node;
 import com.demo.Queue;
 import com.demo.Stack;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        Stack stack=new Stack();
-        Queue queue=new Queue();
-        System.out.println("tescase1: melihat stack kosong atau tidak");
+        Stack stack = new Stack();
+        Queue queue = new Queue();
+        System.out.println("Test Case 1: Melihat stack kosong atau tidak.");
         System.out.println(stack.hasPop());
-        System.out.println("tascase2: pop value");
-        System.out.println(stack.pop());
-        stack.hasPop();
-        stack.push(0);
-        stack.pop();
-        
+        // System.out.println("Test Case 2: Pop value saat stack kosong, output throw
+        // exception.");
+        // System.out.println(stack.pop());
+        System.out.println("Test Case 3: Push value 3 value ke stack.");
+        stack.push(5);
+        stack.push(6);
+        stack.push(7);
+        stack.push(8);
+        System.out.println("Values in stack: " + stackToString(stack));
 
+        System.out.println("Test Case 4: Swap stack berdasar index.");
+        stack.swap(0, 2);
+        System.out.println(stack.toString());
 
+        System.out.println("Test Case 5: Pop value dari stack sampai habis.");
+        while (stack.hasPop()) {
+            System.out.println("Sisa value: " + stackToString(stack));
+            stack.pop();
+            System.out.println("Masih bisa di pop?" + stack.hasPop());
+            if (!stack.hasPop()) {
+                System.out.println("Sudah habis");
+            }
+        }
     }
+
+    public static String stackToString(Stack stack) {
+        StringBuilder sb = new StringBuilder("[");
+        Node current = stack.getFirst();
+        while (current != null) {
+            sb.append(current.getValue());
+            current = current.getNext();
+            if (current != null) {
+                sb.append(", ");
+            }
+        }
+        sb.append("]");
+        return sb.toString();
+    }
+
 }
--- src/main/java/com/demo/Stack.java
+++ src/main/java/com/demo/Stack.java
@@ -32,48 +32,50 @@ public class Stack {
             System.out.println("Posisi harus positif");
             return;
         }
-    
+
         if (pos1 == pos2) {
             System.out.println("\nPosisi sama sehingga tidak ada perubahan");
             return;
         }
-    
+
         Node prev1 = null;
         Node current1 = first;
         for (int i = 0; i < pos1 && current1 != null; i++) {
             prev1 = current1;
             current1 = current1.getNext();
         }
-    
+
         Node prev2 = null;
         Node current2 = first;
         for (int i = 0; i < pos2 && current2 != null; i++) {
             prev2 = current2;
             current2 = current2.getNext();
         }
-    
+
         if (current1 == null || current2 == null) {
             throw new IllegalArgumentException("Posisi diluar jumlah index");
         }
-    
+
         // Pertukaran nilai di posisi pertama dan kedua
         if (prev1 != null) {
             prev1.setNext(current2);
         } else {
             first = current2;
         }
-    
+
         if (prev2 != null) {
             prev2.setNext(current1);
         } else {
             first = current1;
         }
-    
+
         Node temp = current1.getNext();
         current1.setNext(current2.getNext());
         current2.setNext(temp);
     }
 
-    
-    
+    public Node getFirst() {
+        return first;
+    }
 }
+
